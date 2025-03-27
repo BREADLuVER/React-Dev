c34a9171e29f0d2d7c07dbd2fea631a817390b60
BREADLuVER <ry2050@nyu.edu>
2025-03-26 23:19:34 -0400

    added localstorage

--- src/Mocks/FitnessGoalTracker/FitnessContext.jsx
+++ src/Mocks/FitnessGoalTracker/FitnessContext.jsx
@@ -3,21 +3,27 @@ import React, { createContext, useReducer } from "react";
 const FitnessContext = createContext();
 
 const initialState = {
-  goals: [],
+  goals: JSON.parse(localStorage.getItem("fitnessGoals")) || [],
 };
 
 const fitnessReducer = (state, action) => {
+  let updatedGoals;
+
   switch (action.type) {
     case "ADD_GOAL": {
+      updatedGoals = [...state.goals, action.payload]
+      localStorage.setItem("fitnessGoals", JSON.stringify(updatedGoals))
       return {
         ...state,
-        goals : [...state.goals, action.payload],
+        goals : updatedGoals,
       };
     }
     case "REMOVE_GOAL": {
+        updatedGoals = state.goals.filter(goal => goal.id !== action.payload)
+        localStorage.setItem("fitnessGoals", JSON.stringify(updatedGoals))
         return {
             ...state,
-            goals : state.goals.filter(goals => goals.id !== action.payload)
+            goals : updatedGoals
         }
     }
     default: {
