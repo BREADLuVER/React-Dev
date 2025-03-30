d3d97650bd14466c9c427b44d7f48f9ed237bef1
BREADLuVER <ry2050@nyu.edu>
2025-03-30 12:22:53 -0400

    debug todolist

--- src/Debugging/1/App.jsx
+++ src/Debugging/1/App.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useContext, useReducer } from "react";
+import React, { useState } from "react";
 import { createStore } from "redux";
 import { Provider, useDispatch, useSelector } from "react-redux";
 
+// Initial state is incomplete on purpose
 const initialState = {
-  todos: [],
+  todos:[],
 };
 
+// Reducer with bug: forgets to return default state properly
 function todoReducer(state = initialState, action) {
   switch (action.type) {
     case "ADD_TODO":
+      // Sometimes returns undefined due to malformed state
+      if (!action.payload) return state;
       return {
-        todos: [...state.todos, { text: action.payload, completed: false }],
+        todos: [...(state.todos || []), { text: action.payload.text, id: action.payload.id }]
       };
     case "TOGGLE_TODO":
       return {
         ...state,
-        todos: state.todos.map((todo, index) =>
-          index === action.index ? { ...todo, completed: !todo.completed } : todo
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
         ),
       };
     default:
+      // Oops: forgot to return state
       return state;
   }
 }
@@ -27,39 +32,40 @@ function todoReducer(state = initialState, action) {
 const store = createStore(todoReducer);
 
 function TodoInput() {
-  const [input, setInput] = React.useState("");
+  const [input, setInput] = useState("");
   const dispatch = useDispatch();
 
   const handleAdd = () => {
     if (input.trim()) {
-      dispatch({ type: "ADD_TODO", payload: input });
-      setInput(""); // reset input
+      dispatch({
+        type: "ADD_TODO",
+        payload: { text: input,
+            id: Date.now()
+        } // Oops! id is missing here
+      });
+      setInput("");
     }
   };
 
   return (
     <div>
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Enter todo"
-      />
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
       <button onClick={handleAdd}>Add</button>
     </div>
   );
 }
 
 function TodoList() {
-  const todos = useSelector((state) => state.todos);
+  const todos = useSelector((state) => state.todos || []); // Can be undefined
 
   const dispatch = useDispatch();
 
   return (
     <ul>
-      {todos.map((todo, i) => (
-        <li key={i} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
           {todo.text}
-          <button onClick={() => dispatch({ type: "TOGGLE_TODO", index: i })}>
+          <button onClick={() => dispatch({ type: "TOGGLE_TODO", id: todo.id })}>
             Toggle
           </button>
         </li>
@@ -71,7 +77,7 @@ function TodoList() {
 function TodoApp() {
   return (
     <Provider store={store}>
-      <h1>Todo App</h1>
+      <h1>Debuggable Todo App</h1>
       <TodoInput />
       <TodoList />
     </Provider>
